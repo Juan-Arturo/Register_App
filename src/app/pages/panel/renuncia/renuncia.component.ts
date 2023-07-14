caafb4df52fb37328180b776d07ae6aa758d66b2
Juan-Arturo <137929583+Juan-Arturo@users.noreply.github.com>
2023-07-14 12:56:37 -0600

    Add files via upload

---
+++ src/app/pages/panel/renuncia/renuncia.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-renuncia',
+  templateUrl: './renuncia.component.html',
+  styleUrls: ['./renuncia.component.css']
+})
+export class RenunciaComponent {
+
+}
+

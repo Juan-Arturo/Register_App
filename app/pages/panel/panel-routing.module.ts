f9d366009ded62a4b3ca9c8f24d087031c560a89
Juan-Arturo <137929583+Juan-Arturo@users.noreply.github.com>
2023-07-18 10:04:01 -0600

    nueva actualizacion

---
+++ app/pages/panel/panel-routing.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PanelComponent } from './panel.component';
+
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PanelComponent,
+    children: [
+      {
+        path: '', redirectTo: 'registro', pathMatch: 'full'
+      },
+      {
+        path: 'registro',
+        loadChildren: () => import('./Registro/registro.module')
+          .then(i => i.RegistroModule)
+      },
+      {
+        path: 'subsanacion',
+        loadChildren: () => import('./subsana/subsanacion.module')
+          .then(u => u.SubsanacionModule)
+      },
+      {
+        path: 'sustitucion',
+        loadChildren:() => import("./sustitucion/sustitucion.module")
+        .then(c => c.SustitucionModule)
+      },
+      {
+        path: 'renuncia',
+        loadChildren:() => import("./renuncia/renuncia.module")
+        .then(c => c.RenunciaModule)
+      },
+      {
+        path: 'reportes',
+        loadChildren:() => import("./reportes/reportes.module")
+        .then(c => c.ReportesModule)
+      },
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PanelRoutingModule { }

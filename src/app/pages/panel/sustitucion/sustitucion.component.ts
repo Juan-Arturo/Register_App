1f2b1c13c5fde044c096f3b70dfcac74281b7d1c
Juan-Arturo <137929583+Juan-Arturo@users.noreply.github.com>
2023-07-25 15:49:32 -0600

    Update sustitucion.component.ts

--- src/app/pages/panel/sustitucion/sustitucion.component.ts
+++ src/app/pages/panel/sustitucion/sustitucion.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef} from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-sustitucion',
@@ -6,6 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./sustitucion.component.css']
 })
 export class SustitucionComponent {
+  vdate: string; /*variable que almacena dia actual*/ 
+
+  
+  @ViewChild('content', { static: false }) modalContent!: ElementRef; // Definimos una referencia al ElementRef
+
+  constructor(private modalService: NgbModal) {
+    
+    this.vdate = this.getToday();
+  }
+
+  
+  
+  openModal() {
+    this.modalService.open(this.modalContent, { centered: true, size: 'lg' });
+  }
+
+  getToday(): string {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
+
 
 }
+
 

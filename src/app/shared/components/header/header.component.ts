9c2cea24ddc932fdf0a2535f5184b03a307cf1b0
abrahammendez11 <88817975+abrahammendez11@users.noreply.github.com>
2023-07-27 11:04:04 -0600

    boton cierre

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  @ViewChild('content', { static: false }) modalContent!: ElementRef; // Definimos una referencia al ElementRef
+
+  constructor(private modalService: NgbModal) {}
+
+  openModal() {
+    this.modalService.open(this.modalContent, { centered: true, size: 'lg' });
+  }
 
 }
